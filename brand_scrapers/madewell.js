2f8f4f24056f970f20debe5cecbeb39f682bda6e
CeliaYao329 <ycelia29@gmail.com>
2020-05-14 01:32:05 -0400

    Add madewell

---
+++ brand_scrapers/madewell.js
@@ -0,0 +1,37 @@
+function madewell_scraper() {
+    console.assert(document.querySelectorAll(".product-variations .attribute").length == 2, "Madewell: Unexpected");
+    
+    var selected_color;
+    let selected_color_dom = document.querySelector(".product-variations .attribute .selected-value");
+    if (selected_color_dom) {
+        selected_color = {
+            name: selected_color_dom.innerText,
+            code: "0"
+        }
+    }
+
+    var selected_size;
+    let selected_size_dom = document.querySelectorAll(".product-variations .attribute")[1].querySelector(".selectable.selected");
+    let extension_size_dom = document.querySelector(".extended-sizing");
+    if(selected_size_dom) {
+        if(extension_size_dom){
+            let selected_extension_dom=extension_size_dom.querySelector(".extended-sizing-tile.selected");
+            if(selected_extension_dom) {
+                selected_size = selected_size_dom.innerText + " " + selected_extension_dom.innerText; 
+            } 
+        } else {
+            selected_size = selected_size_dom.innerText;
+        }
+    }
+
+    var product_name = document.querySelector(".product-name").innerText;
+    var img_src = document.querySelector(".product-image-wrapper img").getAttribute("data-src")
+
+    var product = {
+        selected_color: selected_color,
+        selected_size: selected_size,
+        product_name: product_name,
+        img_src: img_src
+    }
+    return (product);
+}
